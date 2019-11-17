a7d224f1b9b2ad13c4883f1f5d50ce1eecd590d4
gimhafnu <nifgana@gmail.com>
2019-11-17 16:44:36 +0700

    update battle, harusnya tinggal yang maksa player ngeloop
    @_@
    Kalau mau ditest juga, tolong

--- battle.pl
+++ battle.pl
@@ -1,25 +1,69 @@
 :- debug.
 :- include('tokemon.pl').
+:- include('inventory.pl').
 
 % some dynamic variables
+:- dynamic(game_state/1). game_state(battle).
+
+
 :- dynamic(player_tokemon_list/1). player_tokemon_list([]).
 :- dynamic(player_tokemon_health_list/1). player_tokemon_health_list([]).
 :- dynamic(player_tokemon_enhancemnt_list/1). player_tokemon_enhancemnt_list([]).
 
 :- dynamic(player_inventory/3).  	player_inventory([player_tokemon_list, player_tokemon_health_list, player_tokemon_enhancemnt_list]).
+
 :- dynamic(player_battle_tokemon/1).  	player_battle_tokemon(none).
 :- dynamic(picked_tokemon_num/1). picked_tokemon_num(0).
 :- dynamic(picked_tokemon_health/1). picked_tokemon_health(0).
+:- dynamic(picked_tokemon_enhancment/1). picked_tokemon_enhancment([]).
 :- dynamic(picked_tokemon_used_skill/1). picked_tokemon_used_skill(0).
 
-:- dynamic(opposing_tokemon/1). opposing_tokemon(none).
+:- dynamic(opposing_tokemon/1). opposing_tokemon.
 :- dynamic(opposing_tokemon_health/1). opposing_tokemon_health(0).
 :- dynamic(wild_encountered_tokemon/1). wild_encountered_tokemon(none).
 :- dynamic(opposing_tokemon_used_skill/1). opposing_tokemon_used_skill(0).
 
+:- dynamic(enemy_waiting_list/1). enemy_waiting_list([]).
 
 
 %player_inventory(Tokemon_List, Tokemon_Health_List, Tokemon_Enhancement_List)
+
+% These commands are exposed (used by the player in the game).
+
+specialAttack:-
+    picked_tokemon_used_skill(PT_skill),
+    PT_skill is 0, !,
+    player_battle_tokemon(PBT),
+    format("~p used its skill!", [PBT]);
+    player_skill.
+
+specialAttack:-
+    picked_tokemon_used_skill(PT_skill),
+    PT_skill \== 0, !,
+    player_battle_tokemon(PBT),
+    format("~p is too tired to use another skill!", [PBT]).
+
+pick(Tokemon):-
+    player_tokemon_list(PTL),
+    tokemon_picker(Tokemon, PTL, 1).
+
+
+
+
+
+
+
+
+encounter_tokemon(Wild_tokemon, Waiting_list):-
+    retract(wild_encountered_tokemon(_)),
+    retract(enemy_waiting_list(_)),
+    assertz(wild_encountered_tokemon(Wild_tokemon)),
+    assertz(enemy_waiting_list(Waiting_list)),
+    retract(game_state(_)),
+    assertz(game_state(encounter)).
+    
+    
+
 
 get_tokemon_list_from_inventory(Tokemon_List):-
     player_inventory([TK_List_1|TK_List_2], [HL_List_1|HL_List_2], [EN_List_1|EN_List_2]),
@@ -34,22 +78,24 @@ get_enhancement_list_from_inventory(Enhancement_List):-
     player_inventory([TK_List_1|TK_List_2], [HL_List_1|HL_List_2], [EN_List_1|EN_List_2]),
     append([EN_List_1], EN_List_2, Enhancement_List).
 
+
 initalize_battle(PTOKEMON, Enemy_tokemon):-
     retract(player_battle_tokemon(_)),
-    assert(player_battle_tokemon(PTOKEMON)),
+    assertz(player_battle_tokemon(PTOKEMON)),
     retract(picked_tokemon_health(_)),
     health(PTHEALTH, PTOKEMON),
     format("PHealth    : ~p~n", [PTHEALTH]),
-    assert(picked_tokemon_health(PTHEALTH)),
+    assertz(picked_tokemon_health(PTHEALTH)),
     retract(opposing_tokemon(_)),
-    assert(opposing_tokemon(Enemy_tokemon)),
+    write("ROT"),
+    assertz(opposing_tokemon(Enemy_tokemon)),
     retract(opposing_tokemon_health(_)),
     health(EHEALTH, Enemy_tokemon),
     format("EHealth    : ~p~n", [EHEALTH]),
-    assert(opposing_tokemon_health(EHEALTH)).
-    
-pick(Tokemon):-
-    tokemon_picker(Tokemon, player_inventory, 1).
+    assertz(opposing_tokemon_health(EHEALTH)).
+	
+			   
+												 
 
 tokemon_picker(Tokemon, [], N):- %Reached end of Tokemon List
     write("You do not have that Tokemon."), nl, !.
@@ -58,7 +104,7 @@ tokemon_picker(Tokemon, Inventory_List, N):- %Head is the picked tokemon
     [Head|Tail] = Inventory_List,
     Tokemon == Head, !,
     retract(picked_tokemon_num(M)),
-    assert(picked_tokemon_num(N)),
+    assertz(picked_tokemon_num(N)),
     wild_encountered_tokemon(Current_Foekemon),
     initalize_battle(Tokemon,Current_Foekemon).
 
@@ -71,7 +117,7 @@ tokemon_picker(Tokemon, Inventory_List, N):- %Head is NOT the picked tokemon
 get_type_modifier(Attacker, Attackee, Modifier):-
     tipe(TAR, Attacker), tipe(TAE, Attackee), lawan(TAR, TAE),!, Modifier is rational(1.5); Modifier is rational(1).
 
-player_attack():-
+player_attack:-
     player_battle_tokemon(PBT),
     opposing_tokemon(OT),
     na(PBT, PNA),
@@ -86,13 +132,17 @@ player_attack():-
     Neo_o_hl is (Old_o_hl - DMG),
     format("Neo_o_hl    : ~p~n", [Neo_o_hl]),
     retract(opposing_tokemon_health(_)),
-    assert(opposing_tokemon_health(Neo_o_hl)).
+    assertz(opposing_tokemon_health(Neo_o_hl)),
+    check_enemy.
 
-player_skill():-
+player_skill:-
+    picked_tokemon_used_skill(PTSS),
+    PTSS == 0,
     player_battle_tokemon(PBT),
     opposing_tokemon(OT),
     skill(PBT, PSA),
     format("PSA    : ~p~n", [PSA]),
+    format("~p used skill!~n", [PBT]),
     %insert enhancment modifier getter here pls
 
     get_type_modifier(PBT, OT, Type_Mod),
@@ -103,19 +153,41 @@ player_skill():-
     Neo_o_hl is (Old_o_hl - DMG),
     format("Neo_o_hl    : ~p~n", [Neo_o_hl]),
     retract(opposing_tokemon_health(_)),
-    assert(opposing_tokemon_health(Neo_o_hl)).
+    assertz(opposing_tokemon_health(Neo_o_hl)),
+    picked_tokemon_used_skill(_),
+    picked_tokemon_used_skill(1),
+    check_enemy.
 
-check_enemy():-
+check_enemy:-
     opposing_tokemon_health(Enemy_Health),
-    Enemy_Health > 0, !,
+    Enemy_Health > 0,
     random_between(0, 11, Rando),
-    Rando > 9, !,
-    (opposing_tokemon_used_skill == 0,
-        enemy_skill();
-        enemy_attack());
-    (enemy_attack()).
+    format("Rando    : ~p~n", [Rando]),
+    write("RANDOMISASI"), nl,
+    check_enemy_skill(Rando).
 
-enemy_attack():-
+check_player_battle_tokemon :-
+    picked_tokemon_health(Player_Battle_Health),
+    Player_Battle_Health < 1,
+    kill_player_tokemon.
+
+check_enemy:-
+    opposing_tokemon_health(Enemy_Health),
+    Enemy_Health =< 0,
+    kill_enemy.
+
+check_enemy_skill(Random_Number):-
+    Random_Number > 8,
+    opposing_tokemon_used_skill(OTSS),
+    OTSS == 0,
+    enemy_skill,
+    retract(opposing_tokemon_used_skill(_)),
+    assertz(opposing_tokemon_used_skill(1)).
+
+check_enemy_skill(Random_Number):-
+    enemy_attack.
+
+enemy_attack:-
     player_battle_tokemon(PBT),
     opposing_tokemon(OT),
     na(OT, OTNA),
@@ -130,13 +202,14 @@ enemy_attack():-
     Neo_p_hl is (Old_p_hl - DMG),
     format("Neo_p_hl    : ~p~n", [Neo_p_hl]),
     retract(picked_tokemon_health(_)),
-    assert(picked_tokemon_health(Neo_p_hl)).
+    assertz(picked_tokemon_health(Neo_p_hl)).
 
-enemy_skill():-
+enemy_skill:-
     player_battle_tokemon(PBT),
     opposing_tokemon(OT),
     skill(OT, OTSA),
     format("OTSA    : ~p~n", [OTSA]),
+    format("~p used skill!~n", [OT]),
     %insert enhancment modifier getter here pls
 
     get_type_modifier(OT, PBT, Type_Mod),
@@ -147,5 +220,85 @@ enemy_skill():-
     Neo_p_hl is (Old_p_hl - DMG),
     format("Neo_p_hl    : ~p~n", [Neo_p_hl]),
     retract(picked_tokemon_health(_)),
-    assert(picked_tokemon_health(Neo_p_hl)).
+    assertz(picked_tokemon_health(Neo_p_hl)).
     
+kill_enemy:-
+    enemy_waiting_list(EWL),
+    EWL == [],
+    write("YOU WON!"), nl,
+    player_wins_battle.
+
+kill_enemy:-
+    enemy_waiting_list(EWL),
+    EWL,
+    [Next_Foekemon|Rest_of_Waiting] is EWL,
+    format("~p leaped out of its hiding place after its friend was felled!~n", [Next_Foekemon]),
+    enemy_go_to_next_tokemon.
+
+player_wins_battle:-
+    write("YOU CUKCKING WON!"), nl.
+
+player_set_battle_tokemon_to_inventory:-
+    player_tokemon_health_list(PTHL),
+    picked_tokemon_health(PTH),
+    picked_tokemon_num(TK_NUM),
+    player_tokemon_enhancemnt_list(PTEL),
+    replace(TK_NUM, 1, PTH, PTHL, NEO_PTHL),
+    retract(player_tokemon_health_list(_)),
+    assertz(player_tokemon_health_list(NEO_PTHL)),
+    
+    replace(TK_NUM, 1, [], PTEL, NEO_PTEL),
+    retract(player_tokemon_enhancemnt_list(_)),
+    assertz(player_tokemon_enhancemnt_list(NEO_PTEL)).
+
+player_switch_tokemon(NEO_TKMN_NUMBER):-
+    player_tokemon_list(PTL),
+    player_tokemon_health_list(PTHL),
+    player_tokemon_enhancemnt_list(PTEL),
+
+    get_item_num(PTL, NEO_TKMN_NUMBER, 1, PTOKEMON),
+
+    retract(player_battle_tokemon(_)),
+    assertz(player_battle_tokemon(PTOKEMON)),
+
+    get_item_num(PTHL, NEO_TKMN_NUMBER, 1, PTHEALTH),
+    retract(picked_tokemon_health(_)),
+    format("PHealth    : ~p~n", [PTHEALTH]),
+    assertz(picked_tokemon_health(PTHEALTH)),
+
+    get_item_num(PTEL, NEO_TKMN_NUMBER, 1, PENHANCEMNT),
+    retract(picked_tokemon_enhancment(_)),
+    assertz(picked_tokemon_enhancment(PENHANCEMNT)).
+
+enemy_go_to_next_tokemon:-
+    enemy_waiting_list(EWL),
+    [Head|Tail] is EWL,
+    
+    retract(opposing_tokemon(_)),
+    assertz(opposing_tokemon(Head)),
+
+    health(OTHealth, Head),
+    retract(opposing_tokemon_health(_)),
+    assertz(opposing_tokemon_health(OTHealth)),
+
+    retract(opposing_tokemon_used_skill(_)),
+    assertz(opposing_tokemon_used_skill(0)).
+
+kill_player_tokemon:-
+    picked_tokemon_num(TK_NUM),
+    remove_TK_num_from_inventory(TK_NUM),
+    
+    list_count(player_tokemon_list, NUM_OF_TOKEMON_LEFT),
+    NUM_OF_TOKEMON_LEFT > 0 -> force_player_pick; player_die.
+
+player_die:-
+    retract(opposing_tokemon(_)),
+    retract(opposing_tokemon_health(_)),
+    retract(opposing_tokemon_used_skill(_)),
+    retract(player_battle_tokemon(_)),
+    retract(picked_tokemon_health(_)),
+    retract(picked_tokemon_enhancment(_)).
+
+force_player_pick:- !.
+
+player_ran:- !.
--- inventory.pl
+++ inventory.pl
@@ -1,18 +1,21 @@
 /*Inventory*/
+%:- include(tokemon).
+%:- include(battle).
 
-addTokemon([], TK, [TK]) :- !.
+
+add_to_back_of_list([], TK, [TK]) :- !.
 	/* Basis */
 
-addTokemon([Head|Tail], TK, [Head|Tail2]) :- addTokemon(Tail, TK, Tail2).
+add_to_back_of_list([Head|Tail], TK, [Head|Tail2]) :- add_to_back_of_list(Tail, TK, Tail2).
 	/* Recurrens */
 	
-inv_count([], X) :- X is 0, !.
+list_count([], X) :- X is 0, !.
 	/* Basis */
 
-inv_count([_|TAIL], X) :- inv_count(TAIL, Y), X is Y + 1.
+list_count([_|TAIL], X) :- list_count(TAIL, Y), X is Y + 1.
 	/* Recurrens */
 
-inv_full(Inventory) :- inv_count(Inventory, X), !, X > 5.
+inv_full(Inventory) :- list_count(Inventory, X), !, X > 5.
 
 delTokemon([], _, []) :- !.
 	/* Del object from list : basis */
@@ -23,6 +26,25 @@ delTokemon([A|B], C, B) :- C == A, !.
 delTokemon([A|B], C, [A|D]) :- C \== A, delTokemon(B, C, D).
 	/* Del object from list : recursive - not found */
 
+del_list_num([A|B], N, I, [A|BB]) :- I \== N, J is I + 1, del_list_num(B, N, J, BB).
+	/* Del idx N from a list , not found and continuing */
+
+del_list_num([A|B], N, I, B) :- I == N, !.
+	/* Del idx N from a list , found and stopping */
+
+del_list_num([], N, I, []) :- !.
+	/* Del idx N from a list , not found*/
+
+get_item_num([], N, I, none) :- !.
+get_item_num([H|T], N, I, H) :- I == N.
+get_item_num([H|T], N, I, X) :- I \= N, J is I + 1, get_item_num(T, N, J, X).
+
+
+/* replaces the info of idx N in list to R*/
+replace(_, _, _, [], []).
+replace(N, I, R, [H|T], [R|T2]) :- I == N, replace(N, 999, R, T, T2).
+replace(N, I, R, [H|T], [H|T2]) :- I \= N, J is I + 1, replace(N, J, R, T, T2).
+
 schTK_num([], _, N) :- N is -96,!.
 	%Searches tokemon number in inventory, not found, returns negatives.
 
@@ -32,4 +54,31 @@ schTK_num([A|B], C, N) :- A == C, N is 1,!.
 schTK_num([A|B], C, N) :- schTK_num(B, C, M), N is M + 1.
 	%Searches tokemon number in inventory, not found, continues searching.
 
-add_to_inventory() :- !.
+add_to_inventory(TOKEMON) :-
+	player_tokemon_list(PTL),
+	player_tokemon_health_list(PTHL),
+	player_tokemon_enhancemnt_list(PTEL),
+	add_to_back_of_list(PTL, TOKEMON, NEO_PTL),
+	retract(player_tokemon_list(_)),
+	assertz(player_tokemon_list(NEO_PTL)),
+	health(TKHEL, TOKEMON),
+	add_to_back_of_list(PTHL, TKHEL, NEO_PTHL),
+	retract(player_tokemon_health_list(_)),
+	assertz(player_tokemon_health_list(NEO_PTHL)),
+	add_to_back_of_list(PTEL, [], NEO_PTEL),
+	retract(player_tokemon_enhancemnt_list(_)),
+	assertz(player_tokemon_enhancemnt_list(NEO_PTEL)).
+
+remove_TK_num_from_inventory(TK_NUM) :-
+	player_tokemon_list(PTL),
+	player_tokemon_health_list(PTHL),
+	player_tokemon_enhancemnt_list(PTEL),
+	del_list_num(PTL, TK_NUM, 1, NEO_PTL),
+	retract(player_tokemon_list(_)),
+	assertz(player_tokemon_list(NEO_PTL)),
+	del_list_num(PTHL, TK_NUM, 1, NEO_PTHL),
+	retract(player_tokemon_health_list(_)),
+	assertz(player_tokemon_health_list(NEO_PTHL)),
+	del_list_num(PTEL, TK_NUM, 1, NEO_PTEL),
+	retract(player_tokemon_enhancemnt_list(_)),
+	assertz(player_tokemon_enhancemnt_list(NEO_PTEL)).
